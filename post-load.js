8f09249771b4140533166024c84e9833627fd330
TE-Good <tom.good1990@gmail.com>
2020-09-20 10:29:40 +0100

    Removed button pre-load

--- post-load.js
+++
@@ -1,4 +0,0 @@
-console.log('hello old world')
-
-const getNewReddit = document.querySelector('.redesign-beta-optin')
-getNewReddit.remove()
---
+++ scripts/remove_new_button.js
@@ -0,0 +1,33 @@
+// Taken from https://stackoverflow.com/questions/32533580/deleting-dom-elements-before-the-page-is-displayed-to-the-screen-in-a-chrome-ex
+
+const DEL_SELECTOR = '#redesign-beta-optin-btn';
+
+const observer = new MutationObserver(onMutation);
+// in case the content script was injected after the page is partially loaded
+onMutation([{addedNodes: [document.documentElement]}]);
+observe();
+
+function onMutation(mutations) {
+  const toRemove = [];
+  for (const {addedNodes} of mutations) {
+    for (const n of addedNodes) {
+      if (n.tagName) {
+        if (n.firstElementChild && n.querySelector(DEL_SELECTOR)) {
+          toRemove.push(...n.querySelectorAll(DEL_SELECTOR));
+        }
+      }
+    }
+  }
+  if (toRemove.length) {
+    observer.disconnect();
+    for (const el of toRemove) el.remove();
+    observe();
+  }
+}
+
+function observe() {
+  observer.observe(document, {
+    subtree: true,
+    childList: true,
+  });
+}
