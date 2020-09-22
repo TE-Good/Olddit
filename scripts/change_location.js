8443cccfa1a6bdfc39b6055fcf40001183c1f884
TE-Good <tom.good1990@gmail.com>
2020-09-22 18:19:10 +0100

    Different redirect approach owing to potential edge cases

--- scripts/change_location.js
+++ scripts/change_location.js
@@ -1,3 +1,6 @@
+// Cannot use replace, owing to edge case of targeted word being in URL twice
 const currentLocation = window.location.href
-const newlocation = currentLocation.replace('https://www.reddit.com', 'https://old.reddit.com')
+const locationSplit = currentLocation.split('/')
+locationSplit[2] = "old.reddit.com"
+const newlocation = locationSplit.join('/')
 window.location.assign(newlocation)
