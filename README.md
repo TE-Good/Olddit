a684a2161a4cac010de87f29ffd22093b714997e
TE-Good <tom.good1990@gmail.com>
2020-09-22 18:09:10 +0100

    Fixed redirect

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## PLACEHOLDER
+
+### TODO
+* Get it to work in Firefox
+* Add image to extention
+* Write Up README on how to use
--- scripts/change_location.js
+++ scripts/change_location.js
@@ -1,1 +1,3 @@
-window.location.assign('https://old.reddit.com')
+const currentLocation = window.location.href
+const newlocation = currentLocation.replace('https://www.reddit.com', 'https://old.reddit.com')
+window.location.assign(newlocation)
