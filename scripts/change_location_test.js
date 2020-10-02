7d9fc8283391fc89dbc672a7704352452fc218c3
TE-Good <tom.good1990@gmail.com>
2020-10-02 18:59:06 +0100

    Replaced thumbnail hrefs with old reddit links

--- scripts/change_location_test.js
+++ scripts/change_location_test.js
@@ -1,21 +1,24 @@
 // get all data-type="link"
 const dataType = document.querySelectorAll('*[data-type="link"]')
 const filteredLinks = []
+const filteredTitles = []
 
 dataType.forEach(link => {
   // then filter by data-url
   if (link.attributes.getNamedItem('data-url').value.includes('https://v.redd.it/')) {
+    // Get the old URL
+    const permalinkURL = link.attributes.getNamedItem('data-permalink').value
+    const oldRedditURL = `https://old.reddit.com${permalinkURL}`
     
-    const currentUrl = link.attributes.getNamedItem('data-url').value
-    const prefixURL = 'https://old.reddit.com'
-    const permaURL = link.attributes.getNamedItem('data-permalink').value
-    const combinedURL = `${prefixUrl}${permaUrl}`
-    const output = { tag: link, currentUrl, prefixURL, permaURL, combinedURL}
+    // Replace all thumbnail hrefs
+    const thumbnail = link.childNodes.item(3)
+    thumbnail.href = oldRedditURL
+    console.log({thumbnail})
     
-    // if class includes title
-    // if class is thumbnail
-    // can use data-permalink for the suffix on URL
-
+    // TASK3 - FIND ALL HREFS FOR TITLE
+    // TASK4 - IF THERE IS A RAW HYPERLINK ANYWHERE IN TEXT ON THE PAGE, MAKE SURE TO REPLACE IT (MIGHT NEED REGEX)
+    
+    const output = { tag: link, combinedURL}
     filteredLinks.push(output)
   }
 })
